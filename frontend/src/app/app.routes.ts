2432ffbc7e599487d99c484437a69b3cab5c5683
Manhcuong05 <ngmanhcuong2011@gmail.com>
2026-04-22 19:58:04 +0700

    chore: update and populate node_modules dependencies

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MembersComponent } from './pages/members/members.component';
+import { TreeComponent } from './pages/tree/tree.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'auth', component: AuthComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'members', component: MembersComponent },
+    { path: 'tree', component: TreeComponent }
+];
